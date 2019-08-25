0f2d80b99642a42c94a47e8a7f263f128d9e1e99
jboileau99 <boileaujustin@gmail.com>
2019-08-25 13:38:15 -0600

    Import from VM

---
+++ VA.py
@@ -0,0 +1,22 @@
+# ------------------------------------------------
+#   Name: Ryan Kortbeek / Justin Boileau
+#   ID: 1535901 / 1535268
+#   CCID: rakortbe / jboileau
+#   CMPUT 274, Fall 2018
+#
+#   Project: Virtual Assistant
+#   File: VA.py
+#
+# ------------------------------------------------
+
+import displays
+
+"""
+Driver file used to run program from either terminal or Wing IDE
+"""
+
+if __name__ == "__main__":
+    displays.main()
+    
+else:
+    displays.main()
